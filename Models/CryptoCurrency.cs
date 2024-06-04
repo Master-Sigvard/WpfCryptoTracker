9dfafe0f42b914306504d99d017146bae7b24444
Ivan <datskoivan19@gmail.com>
2024-06-05 02:28:27 +0300

    Initial code
    - Created 'Models' directory for model classes
    - Created 'ViewModels' directory for view model classes
    - Created 'Views' directory for view files
    - Created 'Services' directory for service classes
    - Created 'Resources' directory for resources like styles and localization
    - Created 'Helpers' directory for utility classes
    - Created Helper RelayCommand.cs
    - Created Model CryptoCurrency.cs
    - Created ViewModel ViewModelBase.cs
    - Moved MainWindow.xaml to Views folder

---
+++ Models/CryptoCurrency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCryptoTracker.Models
+{
+    public class CryptoCurrency
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public double CurrentPrice { get; set; }
+        public double MarketCap { get; set; }
+        public double TotalVolume { get; set; }
+        public double PriceChangePercentage24h { get; set; }
+    }
+}
